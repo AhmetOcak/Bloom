69c0e3b9ca11d3b1676eca190b20118eb2c0b38d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-09-27 01:58:52 +0300

    material themes refactored

--- app/src/main/java/com/bloom/ui/theme/Color.kt
+++ app/src/main/java/com/bloom/ui/theme/Color.kt
@@ -2,7 +2,11 @@ package com.bloom.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF4F2C2C)
+val White = Color(0xFFFFFFFF)
+val White150 = Color(0x26FFFFFF)
+val White850 = Color(0xD9FFFFFF)
+val Gray = Color(0xFF232323)
+val Green900 = Color(0xFF2D3B2D)
+val Green300 = Color(0xFFB8C9B8)
--- app/src/main/java/com/bloom/ui/theme/Type.kt
+++ app/src/main/java/com/bloom/ui/theme/Type.kt
@@ -2,27 +2,61 @@ package com.bloom.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.bloom.R
+
+private val font = FontFamily(
+    Font(R.font.nunito_sans_bold, FontWeight.Bold),
+    Font(R.font.nunito_sans_light, FontWeight.Light),
+    Font(R.font.nunito_sans_light, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp,
+        letterSpacing = 0.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontFamily = font,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = font,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = font,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = font,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
     )
-    */
+
 )
