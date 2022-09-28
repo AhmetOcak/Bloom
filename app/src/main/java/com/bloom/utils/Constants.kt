37090b97eabdbe0f72a94ecabfe95c1eaecf32aa
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-09-28 19:27:25 +0300

    nav routes constants added

---
+++ app/src/main/java/com/bloom/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.bloom.utils
+
+object NavRoutes {
+    const val welcomeScreen = "welcome_screen"
+    const val loginScreen = "login_screen"
+    const val homeScreen = "home_screen"
+    const val favoritesScreen = "favorites_screen"
+    const val profileScreen = "profile_screen"
+    const val cartScreen = "cart_screen"
+}
